12685256444e5d01795c0f5425606bb026d1f4f4
Дмитрий Мухин <SawHo@DESKTOP-4H8896D>
2018-11-30 21:40:42 +0700

    Add weather app on react

---
+++ CourseTasks/AppWithReact/React/cities/src/App.js
@@ -0,0 +1,73 @@
+import React from "react";
+import Info from "./components/info"
+import Form from "./components/form"
+import Weather from "./components/weather"
+
+// C:\Users\SawHo>cd C:\Users\SawHo\source\repos\Academit\CourseTasks\AppWithReact\                                                                              React\cities
+// npm start
+
+const ApiKey = "665c465d92324b6d400496d043ad03ff";
+
+class App extends React.Component {
+
+  state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    pressure: undefined,
+    sunset: undefined,
+    error: undefined
+  }
+
+  gettingWeather = async (e) => {
+    e.preventDefault();
+    var city = e.target.elements.city.value;
+
+    if (city) {
+      const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}&units=metric`);
+      const data = await api_url.json();
+
+      var sunset = data.sys.sunset;
+      var date = new Date();
+      date.setTime(sunset);
+      var sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+      this.setState({
+        temp: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        pressure: data.main.pressure,
+        sunset: sunset_date,
+        error: undefined
+      });
+    } else {
+      this.setState({
+        temp: undefined,
+        city: undefined,
+        country: undefined,
+        pressure: undefined,
+        sunset: undefined,
+        error: "Введите название города"
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Info />
+        <Form weatherMethod={this.gettingWeather} />
+        <Weather
+          temp={this.state.temp}
+          city={this.state.city}
+          country={this.state.country}
+          pressure={this.state.pressure}
+          sunset={this.state.sunset}
+          error={this.state.error}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
